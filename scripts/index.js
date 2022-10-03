fd2cec75c600444bf6d4723ade448e722da2c0bd
Maria Tsay <mashuny2@yandex.ru>
2022-10-03 15:32:07 +0300

    ПР 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const POPUP_OPENED_CLASS = 'popup_opened';
 
-
 let popup = document.querySelector('.popup');
 let popupForm = popup.querySelector('.popup__form');
 let openEditBtn = document.querySelector('.profile__profile-info-edit-button');
@@ -9,12 +8,11 @@ let popupSubmitBtn = popupForm.querySelector('.popup__submit-btn');
 let nameInput = popup.querySelector('.popup__text_type_name');
 let jobInput = popup.querySelector('.popup__text_type_about');
 let profileName = document.getElementById('profile-name');
-nameInput.value = profileName.textContent;
 let profileJob = document.getElementById('profile-job');
-jobInput.value = profileJob.textContent;
-
 
 function openPopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
     popup.classList.add(POPUP_OPENED_CLASS);
 }
 
@@ -30,12 +28,13 @@ function closePopup(event) {
 popup.addEventListener('click', closePopup);
 
 
-function popupSubmitHandler(event) {
-    event.preventDefault();
+function popupSubmitHandler(evt) {
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove(POPUP_OPENED_CLASS);
+    if (popupForm.contains(evt.target)) {
+        popup.classList.remove(POPUP_OPENED_CLASS);
+    }
 }
 
 popupForm.addEventListener('submit', popupSubmitHandler);
-
